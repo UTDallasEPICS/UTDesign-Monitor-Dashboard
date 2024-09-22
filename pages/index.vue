db28d4fa3d3aa435752572da240e3b629b1e0866
fegelsamuel <fegelsamuel@gmail.com>
2024-09-21 19:52:54 -0500

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template lang="pug">
+div.mx-auto.mt-7.absolute
+  div.mx-auto.gap-2
+    button
+      NuxtLink(to="/") Home
+    button 
+      NuxtLink(to="/my-dashboards") my dashboards
+    button
+      NuxtLink(to="/shared-dashboards") shared dashboards
+    button
+      NuxtLink(to="/new-dashboard") new dashboard
+    button
+      NuxtLink(to="/admin") Admin
+NuxtPage
+</template>
