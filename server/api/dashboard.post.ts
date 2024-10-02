4729f340048f58184a954bbe5a541cdf8343c204
fegelsamuel <fegelsamuel@gmail.com>
2024-10-02 13:51:50 -0500

    api directory containing dashboard.post.ts added in server directory

---
+++ server/api/dashboard.post.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient() 
+// the first two lines should be at the top of every API file that has anything to do with Prisma
+
+
+
