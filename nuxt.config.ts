724f70c6eb744a541dd679253da2fe93fdd8f2ba
fegelsamuel <fegelsamuel@gmail.com>
2024-10-03 12:34:56 -0500

    nuxt.config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,27 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
+  modules:
+  ['@nuxtjs/tailwindcss',],
+
+  vite: {
+    resolve: {
+      dedupe:["vue"]
+    },
+  },
+function ensureEnv(envName: string): string {
+    if (!process || !process.env) {
+	throw new Error(
+	    'Attempted to get environment variable from client. This should not happen',
+	)
+    }
+    const value = process.env[envName]
+    if (process.env.DISABLE_ENV_CHECKING === 'true') return value!
+    if (!value) {
+    throw new Error(
+	`Missing environment variable "${envName}." Please define it in .env, or use DISABLE_ENV_CHECKING="true"`,
+    )
+	}
+    return value
+    }
 })
--- server/api/dashboard.post.ts
+++ server/api/dashboard.post.ts
@@ -1,6 +1,33 @@
 import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient() 
-// the first two lines should be at the top of every API file that has anything to do with Prisma
 
+export default defineEventHandler(async event => {
+    const body = await readBody(event)
+   /* const replaced_image_cuid = body.replacedImage.cuid
+    if(body.imageUploaded.url != body.replacedImage.url && body.replacedImage.url !== ""){
+    try {
+        // Replaces entry in the database in the image model for a specfic image
+        await prisma.$transaction([
+            prisma.image.delete({
+            where: {
+                cuid: body.imageUploaded.cuid
+            }
+        }), prisma.image.update({
+            where: {
+                cuid: replaced_image_cuid
+            },
+            data: {
+                url:  body.imageUploaded.url,
+        
+                }
+            }
+        ) ]);
 
+        return true;
+    } catch (e) {
+        console.error(e);
+    }
+    }
+    return false
+    */
+})
 
