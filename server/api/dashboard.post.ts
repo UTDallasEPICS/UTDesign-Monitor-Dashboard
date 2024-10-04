2108cc009ce950a25d599445da74abe316771bbb
fegelsamuel <fegelsamuel@gmail.com>
2024-10-04 11:28:49 -0500

    Prisma updated; dashboard.post.ts created

--- server/api/dashboard.post.ts
+++ server/api/dashboard.post.ts
@@ -1,33 +1,29 @@
 import { PrismaClient } from "@prisma/client"
 
 export default defineEventHandler(async event => {
+    const runtime = useRuntimeConfig()
     const body = await readBody(event)
-   /* const replaced_image_cuid = body.replacedImage.cuid
+    const replaced_image_cuid = body.replacedImage.cuid
     if(body.imageUploaded.url != body.replacedImage.url && body.replacedImage.url !== ""){
     try {
-        // Replaces entry in the database in the image model for a specfic image
-        await prisma.$transaction([
-            prisma.image.delete({
-            where: {
-                cuid: body.imageUploaded.cuid
-            }
-        }), prisma.image.update({
-            where: {
-                cuid: replaced_image_cuid
-            },
-            data: {
-                url:  body.imageUploaded.url,
-        
-                }
-            }
-        ) ]);
-
-        return true;
-    } catch (e) {
-        console.error(e);
+	const queryRes = await event.context.client.family.create({
+	    data: {
+		name: name,
+		owner: {
+			connect: { cuid: event.context.user.cuid }
+		    },
+		cuid: undefined,
+		
+	    }
+	}
+	) 
+	return queryRes
+	}
+    catch (e) {
+      console.log(e)
     }
     }
     return false
-    */
+   
 })
 
