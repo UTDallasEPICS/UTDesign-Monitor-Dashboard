f9d1f3cce2dfc8b6c35250f27249e6a7d2d14a0f
paj220001 <paj220001@utdallas.edu>
2024-10-09 15:19:44 -0500

    Set inde.vue to original

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'main.css'  // Adjust the path to where your CSS file is located
-
-createApp(App).mount('#app')
-
 <script setup lang="ts">
+
+
 </script>
 
 <template lang="pug">
 div.mx-auto.mt-7.absolute
   div.mx-auto.gap-2
-    button.bg-gray-200.px-4.py-2
+    button
       NuxtLink(to="/") Home
-    button.bg-gray-200.px-4.py-2
-      NuxtLink(to="/my-dashboards") My Dashboards
-    button.bg-gray-200.px-4.py-2 
-      NuxtLink(to="/shared-dashboards") Shared Dashboards
-    button.bg-gray-200.px-4.py-2 
-      NuxtLink(to="/new-dashboard") New Dashboard
-    button.bg-gray-200.px-4.py-2
+    button 
+      NuxtLink(to="/my-dashboards") my dashboards
+    button
+      NuxtLink(to="/shared-dashboards") shared dashboards
+    button
+      NuxtLink(to="/new-dashboard") new dashboard
+    button
       NuxtLink(to="/admin") Admin
-  NuxtPage
+NuxtPage
 </template>
-
-
