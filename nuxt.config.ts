b1877a6739964a71f3fbbe7dbb6aabaa1921ddbc
FegelSamuel <fegelsamuel@gmail.com>
2024-10-12 21:30:28 -0500

    Local machine rejecting Allison's .nuxtconfig; Copying Pranav's

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default {
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-  modules:
-  ['@nuxtjs/tailwindcss',],
-
-  vite: {
-    resolve: {
-      dedupe:["vue"]
-    },
-  },
-function ensureEnv(envName: string): string {
-    if (!process || !process.env) {
-	throw new Error(
-	    'Attempted to get environment variable from client. This should not happen',
-	)
-    }
-    const value = process.env[envName]
-    if (process.env.DISABLE_ENV_CHECKING === 'true') return value!
-    if (!value) {
-    throw new Error(
-	`Missing environment variable "${envName}." Please define it in .env, or use DISABLE_ENV_CHECKING="true"`,
-    )
-	}
-    return value
-    }
-})
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss']
+}
