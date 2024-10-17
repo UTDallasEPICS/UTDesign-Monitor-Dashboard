8a79a268107babe0285478dd4e4975f17a6166e0
harshptl05 <harshptl2023@gmail.com>
2024-10-16 23:19:05 -0500

    updated nuxt auth files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  modules: ['@nuxt/axios', '@nuxtjs/auth-next'],
+
+  auth: {
+    strategies: {
+      auth0: {
+        domain: 'dev-iogq1tl6zdurzsq2.us.auth0.com',
+        clientId: 'Hfh8GDZlysdv23q8GxOyDaSRJrYbIvto',
+        logoutRedirectUri: 'http://localhost:3000'
+      }
+    }
+  },
+
+  axios: {
+    baseURL: 'http://localhost:3000',
+  },
+  
+  router: {
+    middleware: ['auth']
+  }
+});
---
+++ pages/authbuttons.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <button @click="$auth.loginWith('auth0')">Login with Auth0</button>
+      <button v-if="$auth.loggedIn" @click="$auth.logout()">Logout</button>
+      <p v-if="$auth.loggedIn">Hello, {{ $auth.user.name }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    computed: {
+      isAuthenticated() {
+        return this.$auth.loggedIn;
+      },
+      user() {
+        return this.$auth.user;
+      }
+    }
+  }
+  </script>
+  
