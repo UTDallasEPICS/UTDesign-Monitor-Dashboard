32333b06c0185e81d4cb77c94daa2cd41ee382c5
fegelsamuel <fegelsamuel@gmail.com>
2024-10-19 15:12:01 -0500

    nuxt config, auth0, and login.get.ts copies CV

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default {
+export default defineNuxtConfig({
+  runtimeConfig: {
+    AUTH0_CLIENTID: '',
+    AUTH0_SECRET: '',
+    BASEURL: 'asd',
+    ISSUER: '',
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss']
-}
+})
--- server/api/login.get.ts
+++ server/api/login.get.ts
@@ -1,7 +1,10 @@
+import { loginRedirectUrl } from "./auth0"
+
+
 
 
 export default defineEventHandler((event) => {
     const auth0Url = `https://${process.env.AUTH0_DOMAIN}/authorize?response_type=code&client_id=${process.env.AUTH0_CLIENT_ID}&redirect_uri=${process.env.AUTH0_CALLBACK_URL}&scope=openid profile email`
-    return sendRedirect(event, auth0Url)
+    return sendRedirect(event, loginRedirectUrl() || "")
   })
   
