c8791754584551abb762b9443e909ebbd01b1120
Madeline Scogin <madeline.scogin@gmail.com>
2024-10-30 11:52:25 -0500

    header thing

---
+++ components/MDHeader.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template lang="pug">
+    div.mx-auto.mt-20.absolute
+      div.mx-auto.gap-2
+        button.bg-gray-200.px-4.py-2
+          NuxtLink(to="/") Home
+        button.bg-gray-200.px-4.py-2
+          NuxtLink(to="/my-dashboards") My Dashboards
+        button.bg-gray-200.px-4.py-2 
+          NuxtLink(to="/shared-dashboards") Shared Dashboards
+        button.bg-gray-200.px-4.py-2 
+          NuxtLink(to="/new-dashboard") New Dashboard
+        button.bg-gray-200.px-4.py-2
+          NuxtLink(to="/admin") Admin
+      NuxtPage
+    </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'main.css'  // Adjust the path to where your CSS file is located
+
+
 createApp(App).mount('#app')
 <script setup lang="ts">
-</script>
 
-<template lang="pug">
+
+</script>
+<template>
+  <div>
+    <MDHeader />
+  </div>
+</template>
+<!-- <template lang="pug">
 div.mx-auto.mt-20.absolute
   div.mx-auto.gap-2
     button.bg-gray-200.px-4.py-2
@@ -19,4 +27,4 @@ div.mx-auto.mt-20.absolute
     button.bg-gray-200.px-4.py-2
       NuxtLink(to="/admin") Admin
   NuxtPage
-</template>
+</template> -->
