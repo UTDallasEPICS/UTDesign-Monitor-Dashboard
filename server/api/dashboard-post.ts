ff776096675bc2b9f7c601e7c9fd487c82f17bf0
fegelsamuel <fegelsamuel@gmail.com>
2024-11-03 16:23:44 -0600

    findFirst spelled wrong, needed camel case; forgot to change page to dashboard, fixed; tested with Postman

--- server/api/dashboard-post.ts
+++ server/api/dashboard-post.ts
@@ -8,13 +8,12 @@ export default defineEventHandler(async event => {
     const body = await readBody(event);
     try {
         // vscode is complaining about client, but it should work because client is created in server/middleware/auth.ts
-        const owner = await event.context.client.user.FindFirst({
+        const owner = await event.context.client.user.findFirst({
             where: { cuid: event.context.user?.cuid as string }
         })
-        const queryRes = await event.context.client.page.create({
+        const queryRes = await event.context.client.dashboard.create({
             data: {
-                userCuid: owner.cuid,
-                owner: owner
+                userCuid: owner.cuid
             }
         }) 
         return true
