d58c7b54a1c11b9b179f9742ab7be08b9f04b759
fegelsamuel <fegelsamuel@gmail.com>
2024-11-05 10:35:23 -0600

    shared dashboard button removed

--- components/MDHeader.vue
+++ components/MDHeader.vue
@@ -8,8 +8,6 @@
         button.bg-gray-200.px-4.py-2
           NuxtLink(to="/my-dashboards") My Dashboards
         button.bg-gray-200.px-4.py-2 
-          NuxtLink(to="/shared-dashboards") Shared Dashboards
-        button.bg-gray-200.px-4.py-2 
           NuxtLink(to="/new-dashboard") New Dashboard
         button.bg-gray-200.px-4.py-2
           NuxtLink(to="/admin") Admin
