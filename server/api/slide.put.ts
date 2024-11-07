304b797352b2dcc2d55d6ff57a8089e795e5cadc
fegelsamuel <fegelsamuel@gmail.com>
2024-11-07 12:46:04 -0600

    finished

--- server/api/slide.put.ts
+++ server/api/slide.put.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 
 export default defineEventHandler(async event => {
-    const body = readBody(event)
     const { slideData, newURL, type }: { slideData: any, newURL: any, type: string } = await readBody(event)
     if (type === "image") {
         const result = await event.context.client.slide.update({ 
