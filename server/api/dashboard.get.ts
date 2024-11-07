b2c646cccf6ad2eeed3211b5c05a4a63c1b5c4d6
Madeline Scogin <madeline.scogin@gmail.com>
2024-11-07 15:34:18 -0600

    changed findFirst to findUnique; changed cuid from any to string; authored by Samuel Ma

--- server/api/dashboard.get.ts
+++ server/api/dashboard.get.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from "@prisma/client"
 import {loginRedirectUrl} from "./auth0"
 
 export default defineEventHandler(async event => { 
-    const { cuid } = getQuery(event) // todo: figure out exactly why this works; is it looking up the cuid?
+    const { cuid }: {cuid: string} = await getQuery(event) 
 
-    const queryRes = await event.context.client.dashboard.findFirst({
-        where: { cuid: cuid as string },
+    const queryRes = await event.context.client.dashboard.findUnique({
+        where: { cuid: cuid as string},
       include: {
         owner: true // this is the many to one relationship Dashboards have with Users (owner)
     }
