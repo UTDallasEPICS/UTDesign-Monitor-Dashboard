7f8c8128d0d43233d116571c637dd5e54c92698a
fegelsamuel <fegelsamuel@gmail.com>
2024-11-11 10:07:40 -0600

    added new duration to slide put ts

--- server/api/slide.put.ts
+++ server/api/slide.put.ts
@@ -1,25 +1,31 @@
 import { PrismaClient } from "@prisma/client"
 
 export default defineEventHandler(async event => {
-    const { slideData, newURL, type }: { slideData: any, newURL: any, type: string } = await readBody(event)
+    const { slideData, newURL, type, newDuration }: { slideData: any, newURL: any, type: string, newDuration: number } = await readBody(event)
     if (type === "image") {
         const result = await event.context.client.slide.update({ 
             where: { cuid: slideData.cuid } ,
-            data: { image: newURL }
+            data: { image: newURL ,
+                duration: newDuration
+             }
     });
     return result;
     }
     else if (type === "video") {
         const result = await event.context.client.slide.update({
             where: { cuid: slideData.cuid} ,
-            data: { video: newURL }
+            data: { video: newURL ,
+                duration: newDuration
+            }
         })
         return result;
     }
     else if (type === "website") {
         const result = await event.context.client.slide.update({
             where: { cuid: slideData.cuid} ,
-            data: { website: newURL }
+            data: { website: newURL ,
+                duration: newDuration
+            }
         })
         return result;
     }
