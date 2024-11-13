94a48559dbd742ba3468c55c060fbafc0d99d812
fegelsamuel <fegelsamuel@gmail.com>
2024-11-13 16:11:57 -0600

    init

---
+++ server/api/User/users.get.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client"
+
+
+export default defineEventHandler(async event =>  {
+    const queryRes = await event.context.client.user.findMany({
+        include: {
+            dashboards: true
+        }
+    })
+    return queryRes
+})
