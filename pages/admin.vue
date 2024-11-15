6fcaabeade1a2637e4a464b77d62e8cc4c2a7868
juhisahay <jsahay750@gmail>
2024-11-14 23:18:40 -0600

    admin page; buttons need alignment

--- pages/admin.vue
+++ pages/admin.vue
@@ -4,19 +4,29 @@
 </script>
 
 <template lang="pug">
-div.mx-auto.mt-7.absolute
-  div.mx-auto.gap-2
-    button.bg-gray-200.px-4.py-2
-        NuxtLink(to="/") Home
-    button.bg-gray-200.px-4.py-2
-        NuxtLink(to="/current-users") Current Users
-    button.bg-gray-200.px-4.py-2
-        NuxtLink(to="/new-user") New User
-    button.bg-gray-200.px-4.py-2
-        NuxtLink(to="/add-admin") Add Admin
-    button.bg-gray-200.px-4.py-2
-        NuxtLink(to="/remove-dashboard") Remove Shared Dashboards
-NuxtPage
+div.bg-purple-200.min-h-screen.w-screen.flex.justify-center.items-center
+    div.bg-purple-200.p-10.bg-white.border.rounded-md.w-full.max-w-3xl
+        div.mx-auto.gap-12
+            div.flex.justify-between.mb-4
+                button.bg-gray-200.px-4.py-2.rounded
+                        NuxtLink(to="/") Home
+                h2.text-xl.font-semibold My Admin Page              
+         
+            div.flex.justify-between.items-center.mb-6
+                div.inline-grid.grid-cols-12.gap-4
+                    button.bg-red-200.col-start-8.col-end-9.bg-gray-200.px-4.py-2.rounded.w-32
+                        NuxtLink(to="/current-users") Current Users
+
+                    button.bg-red-200.col-start-11.col-end-12.bg-gray-200.px-4.py-2.rounded.w-32
+                        NuxtLink(to="/new-user") Add User
+
+            div.mx-auto.gap-12
+                button.bg-purple-200.px-2.py-2.rounded.text-sm.font-semibold
+                    NuxtLink(to="/remove-dashboard") Shared Dashboards
+
+
+    NuxtPage
 </template>
-    
+
+
 
