0da06454862bd54a65f7331ec949522701b299e7
fegelsamuel <fegelsamuel@gmail.com>
2024-11-20 15:19:37 -0600

    backend API fixes

--- server/api/User/user.get.ts
+++ server/api/User/user.get.ts
@@ -2,11 +2,10 @@ import { PrismaClient } from "@prisma/client"
 
 
 export default defineEventHandler(async event =>  {
+    const query = getQuery(event)
     if (event.context.user.user_role === 'admin') {
-    const queryRes = await event.context.client.user.findMany({
-        include: {
-            dashboards: true
-        }
+    const queryRes = await event.context.client.user.findUnique({
+        where: { cuid: query.cuid }
     })
     return queryRes
     }
