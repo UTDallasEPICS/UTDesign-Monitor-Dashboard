df875b7a3f670f2312b9bd9488906a16808351ba
FegelSamuel <fegelsamuel@gmail.com>
2024-11-25 14:41:24 -0600

    fixed the View Dashboard UI

--- pages/Dashboard/[dashboard_id]/index.vue
+++ pages/Dashboard/[dashboard_id]/index.vue
@@ -19,16 +19,14 @@ const videoUrl = ref('https://example.com/sample-video.mp4')  // video URL
 <template lang="pug">
 MDBody
   MDHeader
-  div.bg-purple-200.min-h-screen.w-screen.flex.justify-center.items-center
-      div.bg-white.border-4.border-gray-300.rounded-xl.w-full.max-w-2xl.p-16.shadow-2xl
-          // Slide button (large and centered, now showing link preview)
-          div.slide-button-container.flex.justify-center.items-center.mb-10
-              iframe(:src="previewUrl" class="w-full h-full rounded-lg" allow="fullscreen" frameborder="0")
+  div.bg-white.border-4.border-gray-300.rounded-xl.w-full.max-w-2xl.p-16.shadow-2xl
+    div.slide-button-container.flex.justify-center.items-center.mb-10
+      iframe(:src="previewUrl" class="w-full h-full rounded-lg" allow="fullscreen" frameborder="0")
 
           // Other buttons below
-          div.buttons.flex.justify-between.mt-8
-              button.bg-purple-200.px-8.py-4.rounded-lg.text-xl.font-semibold.w-full.text-center.mr-4 
-                  NuxtLink(to="/EditDashboard/0") Edit
-              button.bg-red-200.px-8.py-4.rounded-lg.text-xl.font-semibold.w-full.text-center 
-                  NuxtLink(to="/Dashboard/0/0") View
+    div.buttons.flex.justify-between.mt-8
+        button.bg-purple-200.px-8.py-4.rounded-lg.text-xl.font-semibold.w-full.text-center.mr-4 
+            NuxtLink(to="/EditDashboard/0") Edit
+        button.bg-red-200.px-8.py-4.rounded-lg.text-xl.font-semibold.w-full.text-center 
+            NuxtLink(to="/Dashboard/0/0") View
 </template>
