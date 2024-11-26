fd7233351f6eb3cd999f4caec91ee292886055c8
FegelSamuel <fegelsamuel@gmail.com>
2024-11-25 21:26:11 -0600

    fixed slide put ts

--- server/api/Slide/slide.put.ts
+++ server/api/Slide/slide.put.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from "@prisma/client"
 
 export default defineEventHandler(async event => {
-    const { slideData, newURL, duration } : { slideData: any, newURL: any, type: string, duration: number } = await readBody(event)
+    const { slideData, newURL, duration } : { slideData: any, newURL: any, type: string, duration: string } = await readBody(event)
     console.log(slideData)
     console.log(duration)
+    const durationAsInt = parseInt(duration, 10)
         const result = await event.context.client.slide.update({ 
             where: { cuid: slideData.cuid },
-            data: { image: newURL, video: newURL, website: newURL, duration: duration }
+            data: { image: newURL, video: newURL, website: newURL, duration: durationAsInt }
         })
         return result;
 });
