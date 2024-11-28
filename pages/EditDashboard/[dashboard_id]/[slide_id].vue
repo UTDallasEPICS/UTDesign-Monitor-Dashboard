6a82361417f55184398364c9271131a80ab8c641
FegelSamuel <fegelsamuel@gmail.com>
2024-11-27 23:44:40 -0600

    unexpected token, expected ","

--- pages/EditDashboard/[dashboard_id]/[slide_id].vue
+++ pages/EditDashboard/[dashboard_id]/[slide_id].vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed } from 'vue'
 import type { Slide } from '@/types.d.ts'
 import { useRoute } from 'vue-router'
 
@@ -16,7 +16,6 @@ const videoLink = ref('https://example.com/sample-video.mp4');
 const isYouTube = ref(false);
 const route = useRoute()
 
-// Full URL
 const fullURL = route.fullPath
 
 const regex = /^.*\/([a-zA-Z0-9]+)\/(\d+)$/ 
@@ -32,6 +31,16 @@ const { data: slideData } = await useFetch<Slide>('/api/Slide/slide', {
     }
 })
 
+// list of all slides in this dashboard, needed for size so we can know if the current slide index is the last one. 
+const { data: slides } = await useFetch<Slide>('/api/Slide/slides', { // linter complaining here, ignore it
+  method: 'GET',
+  query: {
+    dashboardCuid: dashboardCuid
+  }
+})
+
+const lastIndex = slides.value.length // recall that indexes for slides start from 1, not 0
+
 const inputDuration = ref('');
 const duration = ref(slideData.value.duration) || 10
 
@@ -41,10 +50,18 @@ async function updateDuration() {
    duration.value = inputDuration.value
    const saveSuccess  = await $fetch('/api/Slide/slide', { 
          method: 'PUT', 
-         body: ({ slideData: slideData.value, duration: duration.value })
+         body: ({ slideData: slideData.value, duration: duration.value, type: selectedOption }) // the type is for later when typing is implemented for Slides in the schema
      })
   return saveSuccess
 }
+
+async function createSlide() {
+  const saveSuccess = await $fetch('/api/Slide/slide', {
+    method: 'POST',
+    body: ({ dashboardCuid: dashboardCuid })
+  })
+}
+
 
 const inputWebsiteUrl = ref('');
 const websiteUrl = ref('https://example.com');
@@ -80,20 +97,29 @@ function updateWebsiteUrl() {
   MDBody
     MDHeader
     div.bg-white.border-4.border-gray-300.rounded-xl.w-full.max-w-2xl.p-16.shadow-2xl.items-center.flex.flex-col.space-y-4
-      div.mb-8
-        label(for="select-option" class="block text-lg font-semibold mb-2") Select Type:
-        select#select-option(v-model="selectedOption" class="w-full px-4 py-2 border border-gray-300 rounded-lg")
-          option(value="") -- Select --
-          option(value="image") Image
-          option(value="video") Video
-          option(value="website") Website
+      div.grid-cols-3.row-span-1.flex.justify-between.space-x-32
+        button.mt-4.bg-purple-200.px-4.py-1.rounded-lg.text-base.font-semibold.hover_bg-purple-300.transition(v-if="slideIndex > 1") Previous
+          NuxtLink(:to="EditDashboard/${dashboardCuid}/${(slideIndex - 1)}")
+        button.mt-4.bg-purple-200.px-4.py-1.rounded-lg.text-base.font-semibold.hover_bg-purple-300.transition(v-if="slideIndex <= 1") Back
+          NuxtLink(:to="EditDashboard/${dashboardCuid}")
+        div.mb-8
+          label(for="select-option" class="block text-lg font-semibold mb-2") Select Type:
+          select#select-option(v-model="selectedOption" class="w-full px-4 py-2 border border-gray-300 rounded-lg")
+            option(value="") -- Select --
+            option(value="image") Image
+            option(value="video") Video
+            option(value="website") Website
+        button.mt-4.bg-purple-200.px-4.py-1.rounded-lg.text-base.font-semibold.hover_bg-purple-300.transition(@click="createSlide" v-if="slideIndex == lastIndex") New Slide
+          NuxtLink(:to="EditDashboard/${dashboardCuid}/${lastIndex + 1}")
+        button.mt-4.bg-purple-200.px-4.py-1.rounded-lg.text-base.font-semibold.hover_bg-purple-300.transition(v-if="slideIndex != lastIndex") Next
+          NuxtLink(:to="EditDashboard/${dashboardCuid}/${slideIndex + 1}")
   
-      div.mt-8
+      div.mb-8
         // Image Section
         div(v-if="selectedOption === 'image'")
           label(for="image-url" class="block text-lg font-semibold mb-2") Enter Image URL:
           input#image-url(type="text" v-model="inputImageUrl" placeholder="Enter image URL" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-purple-300")
-          button(@click="updateImageUrl" class="mt-4 bg-purple-200 px-4 py-2 rounded-lg text-base font-semibold hover:bg-purple-300 transition") Submit
+          button.hover_bg-purple-300.transition(@click="updateImageUrl" class="mt-4 bg-purple-200 px-4 py-2 rounded-lg text-base font-semibold hover:bg-purple-300 transition") Submit
   
           div.mt-5
             img(:src="imageUrl" alt="" class="w-48 h-auto rounded-lg border border-gray-300")
@@ -102,7 +128,7 @@ function updateWebsiteUrl() {
         div(v-if="selectedOption === 'video'")
           label(for="video-url" class="block text-lg font-semibold mb-2") Enter Video URL:
           input#video-url(type="text" v-model="inputVideoUrl" placeholder="Enter video or YouTube URL" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-purple-300")
-          button(@click="updateVideoLink" class="mt-4 bg-purple-200 px-4 py-2 rounded-lg text-base font-semibold hover:bg-purple-300 transition") Submit
+          button.hover_bg-purple-300.transition(@click="updateVideoLink" class="mt-4 bg-purple-200 px-4 py-2 rounded-lg text-base font-semibold hover:bg-purple-300 transition") Submit
   
           div.relative.flex.items-center.justify-center.h-64.overflow-hidden.mt-8
             iframe(v-if="isYouTube" :src="videoLink" class="w-full h-full border border-gray-300 rounded-lg" allowfullscreen)
@@ -116,13 +142,13 @@ function updateWebsiteUrl() {
               | Note: Websites with ".com" may not work properly.
           label(for="website-url" class="block text-lg font-semibold mb-2") Enter Website URL:
           input#website-url(type="text" v-model="inputWebsiteUrl" placeholder="Enter website URL" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-purple-300")
-          button(@click="updateWebsiteUrl" class="mt-4 bg-purple-200 px-4 py-2 rounded-lg text-base font-semibold hover:bg-purple-300 transition") Submit
+          button.hover_bg-purple-300.transition(@click="updateWebsiteUrl" class="mt-4 bg-purple-200 px-4 py-2 rounded-lg text-base font-semibold hover:bg-purple-300 transition") Submit
   
           div.mt-5.h-72
             iframe(v-if="websiteUrl" :src="websiteUrl" class="w-full h-full border border-gray-300 rounded-lg" allowfullscreen)
       div.mb-8
         label.block.text-lg.font-semibold.mb-2(for="slide-duration") Enter Slide Duration:
         input#slide-duration.w-full.px-4.py-2.border.border-gray-300.rounded-lg(type="text" v-model="inputDuration" placeholder=`Enter in seconds`)
-        button.mt-4.bg-purple-200.px-4.py-2.rounded-lg.text-base.font-semibold(@click="updateDuration") Submit
+        button.hover_bg-purple-300.transition.mt-4.bg-purple-200.px-4.py-2.rounded-lg.text-base.font-semibold(@click="updateDuration") Submit
   </template>
   
