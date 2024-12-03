d3b7efb8c7e33c8b6fb6615bdaa84408c8bd4f09
Samuel Ma <fegelsamuel@gmail.com>
2024-12-03 14:14:33 -0600

    Update MDHeader.vue

--- components/MDHeader.vue
+++ components/MDHeader.vue
@@ -17,7 +17,7 @@ div.mt-7.gap-2.border-4.bg-fuchsia-100.border-gray-300.rounded-xl.shadow-2xl.fle
     a(href="/api/login/login") Login
   // If you're wondering about the extra (), it's because the Tailwind bg wasn't working properly without them
   button.px-4.py-2.shadow-2xl.bg-fuchsia-200.rounded-lg(v-if="isLoggedIn") 
-    NuxtLink(to="/my-dashboards") My Dashboards
+    NuxtLink(to="/my-dashboards") Dashboards
   button.px-4.py-2.shadow-2xl.bg-red-200.rounded-lg(v-if="isAdmin")
     NuxtLink(to="/admin") Admin
   button.px-4.py-2.shadow-2xl.bg-red-100.rounded-lg(v-if="isLoggedIn")
