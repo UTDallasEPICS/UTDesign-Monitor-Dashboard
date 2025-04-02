29601c9300e3446b93022eaa9b4b614fdea3cd22
Hamixer <101950774+Hamixer@users.noreply.github.com>
2025-04-02 15:13:09 -0500

    Lowercase Folder Name

---
+++ pages/dashboard/[dashboard_id]/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const previewUrl = ref("https://en.wikipedia.org/wiki/Main_Page"); // Set the initial preview URL here
+const showDropdown = ref(false); // Toggle for dropdown visibility
+
+// Function to toggle the dropdown visibility
+function toggleDropdown() {
+  showDropdown.value = !showDropdown.value;
+}
+
+const route = useRoute()
+const fullURL = route.fullPath
+const regex = /^.*\/([a-zA-Z0-9]+)\/([a-zA-Z0-9]+)$/ 
+const match = fullURL.match(regex)
+const dashboardCuid = match ? match[2] : null // It grabs EditDashboard for the second index of match, so we want the third index or index #2
+
+
+</script>
+
+<template lang="pug">
+  div.bg-white.border-4.border-gray-300.rounded-xl.w-full.max-w-2xl.p-16.shadow-2xl
+    div.slide-button-container.flex.justify-center.items-center.mb-10
+      iframe(:src="previewUrl" class="w-full h-full rounded-lg" allow="fullscreen" frameborder="0")
+    div.buttons.flex.justify-between.mt-8
+      button.bg-purple-200.px-8.py-4.rounded-lg.text-xl.font-semibold.w-full.text-center.mr-4()
+        NuxtLink(:to="`/EditDashboard/${dashboardCuid}`") Edit
+      button.bg-green-200.px-8.py-4.rounded-lg.text-xl.font-semibold.w-full.text-center()
+        NuxtLink(:to="`/Dashboard/${dashboardCuid}/0`") Present
+</template>
---
+++ pages/edit-dashboard/[dashboard_id]/rename.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+    div.bg-white.border-4.border-gray-300.rounded-xl.w-full.max-w-2xl.p-16.shadow-2xl.justify-center.flex.flex-col.space-y-4
+        div.mb-8
+            label.block.text-lg.font-semibold.mb-2(for="dashboard-name") Enter New Name for Dashboard:
+            input#dashboard-name.w-full.px-4.py-2.border.border-gray-300.rounded-lg(type="text" v-model="newName" placeholder="Enter Dashboard Name")
+            button.mt-4.bg-purple-200.px-4.py-2.rounded-lg.text-base.font-semibold.hover_bg-purple-300.transition(@click="updateNewName")
+                NuxtLink(:to="`/EditDashboard/${dashboardCuid}`") Submit
+</template>
+    
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Dashboard } from '@/types.d.ts' 
+import { useRoute } from 'vue-router'
+
+
+const newName = ref('')
+const route = useRoute()
+const url = route.fullPath
+const regex = /\/EditDashboard\/([a-zA-Z0-9]+)/;
+const match = url.match(regex);
+const dashboardCuid = match ? match[1] : null
+async function updateNewName() {
+    const saveSuccess  = await $fetch('/api/Dashboard/dashboard', { 
+         method: 'PUT', 
+         body: { cuid: dashboardCuid, name: newName.value }
+     })
+    return saveSuccess
+}
+
+
+
+</script>
