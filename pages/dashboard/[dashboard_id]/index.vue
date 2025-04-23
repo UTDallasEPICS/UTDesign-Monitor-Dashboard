66ffc87514535673c10fe6a63a78da6c10f5b152
ssamikshaa <samikshaa.saiprakash@gmail.com>
2025-04-23 13:05:38 -0500

    fixing merging issues

--- pages/dashboard/[dashboard_id]/index.vue
+++ pages/dashboard/[dashboard_id]/index.vue
@@ -16,7 +16,7 @@ const match = url.match(regex);
 const dashboardCuid = match ? match[2] : null; // It grabs EditDashboard for the second index of match, so we want the third index or index #2
 console.log("Dashboard CUID:", dashboardCuid);
 
-const { data: slideData, pending, error } = useFetch('/api/Slide/slide', {
+const { data: slideData, pending, error } = useFetch('/api/slide/slide', {
   query: {
     dashboardCuid: dashboardCuid,
     index: 1,
@@ -34,14 +34,12 @@ watch(slideData, (newSlideData) => {
 </script>
 
 <template lang="pug">
-MDBody
-  MDHeader
   div.bg-white.border-4.border-gray-300.rounded-xl.w-full.max-w-2xl.p-16.shadow-2xl
     div.slide-button-container.flex.justify-center.items-center.mb-10
       iframe(:src="previewUrl" class="w-full h-full rounded-lg" allow="fullscreen" frameborder="0")
     div.buttons.flex.justify-between.mt-8
-        NuxtLink(:to="`/EditDashboard/${dashboardCuid}`")
+        NuxtLink(:to="`/edit-dashboard/${dashboardCuid}`")
           button.bg-purple-200.px-8.py-4.rounded-lg.text-xl.font-semibold.w-full.text-center.mr-4 Edit
-        NuxtLink(:to="`/Dashboard/${dashboardCuid}/0`")
+        NuxtLink(:to="`/dashboard/${dashboardCuid}/0`")
           button.bg-green-200.px-8.py-4.rounded-lg.text-xl.font-semibold.w-full.text-center Present
 </template>
