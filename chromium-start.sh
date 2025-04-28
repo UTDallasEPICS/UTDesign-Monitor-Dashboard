c124d99200d65f487ecc216a7eaecccb167ef761
nama718 <amanbalam817@gmail.com>
2025-04-28 08:36:00 -0500

    Update chromium-start.sh
    Added logs for debugging

--- chromium-start.sh
+++ chromium-start.sh
@@ -1,15 +1,23 @@
+amanb@raspberrypi:~ $ cat chromium-start.sh
 #!/bin/bash
-# Wait for server to be up
+
+# Set DISPLAY
+export DISPLAY=:0
+
+echo "Script started at $(date)" >> /home/amanb/chromium-start.log
+
+# Wait until localhost:3000 is available
 while ! nc -z localhost 3000; do
+  echo "Waiting for server..." >> /home/amanb/chromium-start.log
   sleep 1
 done
 
-# Start Chromium with the desired settings
+echo "Starting Chromium at $(date)" >> /home/amanb/chromium-start.log
+
 /usr/bin/chromium-browser \
   --no-sandbox \
   --disable-dev-shm-usage \
   --start-maximized \
-  --noerrdialogs \
   --disable-infobars \
   --start-fullscreen \
   --disable-software-rasterizer \
@@ -20,4 +28,7 @@ done
   --disable-accelerated-video-encode \
   --disable-accelerated-mjpeg-decode \
   --remote-debugging-port=9222 \
-  http://localhost:3000
+  --disable-extensions \
+  --incognito \
+  http://localhost:3000 >> /home/amanb/chromium-start.log 2>&1
+amanb@raspberrypi:~ $
