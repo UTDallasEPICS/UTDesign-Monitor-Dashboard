d228105e3fff12cdf97e152867f6e9b4c97f2de4
nama718 <amanbalam817@gmail.com>
2025-04-28 08:37:43 -0500

    Update chromium-start.sh
    Now it has an auto restart for chromium after a few seconds 
    
    also got rid of the cat command that I mistakenly added in the file

--- chromium-start.sh
+++ chromium-start.sh
@@ -1,34 +1,51 @@
-amanb@raspberrypi:~ $ cat chromium-start.sh
 #!/bin/bash
 
-# Set DISPLAY
+# Set DISPLAY environment variable
 export DISPLAY=:0
 
-echo "Script started at $(date)" >> /home/amanb/chromium-start.log
+LOG_FILE="/home/amanb/chromium-start.log"
+
+echo "Script started at $(date)" >> "$LOG_FILE"
+
+# Max wait time for server (seconds)
+MAX_WAIT=180
+WAITED=0
 
 # Wait until localhost:3000 is available
 while ! nc -z localhost 3000; do
-  echo "Waiting for server..." >> /home/amanb/chromium-start.log
-  sleep 1
+  echo "Waiting for server... ($WAITED seconds)" >> "$LOG_FILE"
+  sleep 2
+  WAITED=$((WAITED + 2))
+  if [ "$WAITED" -ge "$MAX_WAIT" ]; then
+    echo "Server did not start within $MAX_WAIT seconds. Exiting script." >> "$LOG_FILE"
+    exit 1
+  fi
 done
 
-echo "Starting Chromium at $(date)" >> /home/amanb/chromium-start.log
+echo "Server is ready! Launching Chromium at $(date)" >> "$LOG_FILE"
 
-/usr/bin/chromium-browser \
-  --no-sandbox \
-  --disable-dev-shm-usage \
-  --start-maximized \
-  --disable-infobars \
-  --start-fullscreen \
-  --disable-software-rasterizer \
-  --ignore-gpu-blocklist \
-  --restore-last-session \
-  --disable-gpu \
-  --disable-accelerated-video-decode \
-  --disable-accelerated-video-encode \
-  --disable-accelerated-mjpeg-decode \
-  --remote-debugging-port=9222 \
-  --disable-extensions \
-  --incognito \
-  http://localhost:3000 >> /home/amanb/chromium-start.log 2>&1
-amanb@raspberrypi:~ $
+# Loop Chromium forever
+while true; do
+  echo "Starting Chromium at $(date)" >> "$LOG_FILE"
+  
+  /usr/bin/chromium-browser \
+    --no-sandbox \
+    --disable-dev-shm-usage \
+    --start-maximized \
+    --disable-infobars \
+    --start-fullscreen \
+    --disable-software-rasterizer \
+    --ignore-gpu-blocklist \
+    --restore-last-session \
+    --disable-gpu \
+    --disable-accelerated-video-decode \
+    --disable-accelerated-video-encode \
+    --disable-accelerated-mjpeg-decode \
+    --remote-debugging-port=9222 \
+    --disable-extensions \
+    --incognito \
+    http://localhost:3000 >> "$LOG_FILE" 2>&1
+  
+  echo "Chromium crashed at $(date). Restarting in 5 seconds..." >> "$LOG_FILE"
+  sleep 5
+done
